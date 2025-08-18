3524c9862732010f802917c5ebee215d838fb6de
Daniel Ma <52453932+danielma911@users.noreply.github.com>
2025-08-18 14:43:15 +0800

    Update gcs.tf

--- code/build/gcs.tf
+++ code/build/gcs.tf
@@ -1,35 +1,22 @@
 
 
-required_providers {
-  google = {
-    source  = "hashicorp/google"
-    version = "~> 5.0"
-  }
-}
-
-
 provider "google" {
   project = qwiklabs-gcp-03-fa7edfd03d8e
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "example" {
+resource "google_storage_bucket" "Example" {
   name          = "demo2-${random_id.rand_suffix.hex}"
   location      = "us-central1"
   force_destroy = true
 
   uniform_bucket_level_access = false
-
-  labels = {
-    yor_name  = "example"
-    yor_trace = "5fa511ef-0e81-478b-b04c-e06af9ada4dc"
-  }
 }
 
-resource "random_id" "rand_suffix" {
+resource "random_id" "Rand_suffix" {
   byte_length = 4
 }
 
-output "bucket_name" {
-  value = google_storage_bucket.example.name
+output "Bucket_name" {
+  value = google_storage_bucket.Example.name
 }
