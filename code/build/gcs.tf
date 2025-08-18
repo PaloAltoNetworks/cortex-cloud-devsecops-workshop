df26f6387e78caaf4058087dbd184925a1918234
Daniel Ma <52453932+danielma911@users.noreply.github.com>
2025-08-18 15:03:37 +0800

    Update gcs.tf

--- code/build/gcs.tf
+++ code/build/gcs.tf
@@ -5,30 +5,18 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "example" {
+resource "google_storage_bucket" "Example" {
   name          = "demo2-${random_id.rand_suffix.hex}"
   location      = "us-central1"
   force_destroy = true
 
   uniform_bucket_level_access = false
-
-  labels = {
-    git_commit           = "7f91e0492e5db79bcd3d9f50de95c901aea98526"
-    git_file             = "code__build__gcs_tf"
-    git_last_modified_at = "2025-08-18-06-54-46"
-    git_last_modified_by = "52453932danielma911"
-    git_modifiers        = "52453932danielma911"
-    git_org              = "danielma911"
-    git_repo             = "prisma-cloud-devsecops-workshop"
-    yor_name             = "example"
-    yor_trace            = "9e4caec0-17ce-4cfe-adb7-5f209ced322f"
-  }
 }
 
-resource "random_id" "rand_suffix" {
+resource "random_id" "Rand_suffix" {
   byte_length = 4
 }
 
-output "bucket_name" {
+output "Bucket_name" {
   value = google_storage_bucket.example.name
 }
