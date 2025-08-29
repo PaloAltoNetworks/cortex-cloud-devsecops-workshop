8f60aea3580039cfb29b613b4bc574f35550de40
d-antolinez <david.antolinez@netdatanetworks.com>
2025-08-28 19:47:06 -0500

    Create gcs.tf

---
+++ code/build/gcs.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "example" {
+  name          = "demo-${random_id.rand_suffix.hex}"
+  location      = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = false
+  public_access_prevention    = "enforced"
+}
+
+resource "random_id" "rand_suffix" {
+  byte_length = 4
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.example.name
+}
